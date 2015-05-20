33fa582aa6cca1abd72c9961759cff7e913739ed
Emanuele <feliziani.emanuele@gmail.com>
2015-05-20 12:18:36 +0200

    Add all remaining pages
    Add all remaining pages with styles and animations. Woo!
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+/***************** HELPERS *****************/
+//Element.prototype.addClassC = Element.prototype.addClassC || function ( htmlClass ) {
+//	this.classList.add( htmlClass );
+//	return this;
+//};
+//
+//Element.prototype.removeClassC = Element.prototype.removeClassC || function ( htmlClass ) {
+//	this.classList.remove( htmlClass );
+//	return this;
+//};
+//
+//Element.prototype.toggleClassC = Element.prototype.toggleClassC || function ( htmlClass ) {
+//	this.classList.toggle( htmlClass );
+//	return this;
+//};
+
+
+
 function menuHandler() {
 	var menuTrigger = document.querySelector('.menu-trigger');
 	menuTrigger.addEventListener('click', function() {
@@ -5,6 +23,61 @@ function menuHandler() {
 	}, false);
 }
 
+/***************** FORM ANIMATIONS *****************/
+
+function onInputFocus( ev ) {
+	ev.target.parentNode.classList.add( 'input--filled' );
+}
+
+function onInputBlur( ev ) {
+	if( ev.target.value.trim() === '' ) {
+		ev.target.parentNode.classList.remove( 'input--filled' );
+	}
+}
+
+var allInputs = document.querySelectorAll( 'input:not([type=submit]), textarea' );
+[].forEach.call( allInputs, function ( inputEl ) {
+	// in case the input is already filled
+	if( inputEl.value.trim() !== '' ) {
+		inputEl.parentNode.classList.add( 'input--filled' );
+	}
+
+	// events:
+	inputEl.addEventListener( 'focus', onInputFocus );
+	inputEl.addEventListener( 'blur', onInputBlur );
+} );
+
+/***************** MAMMOCCI *****************/
+var mainMammoccio = document.querySelector( '.main-mammoccio' );
+function updateMammoccio() {
+	var name = this.getAttribute( 'data-mammoccio' ),
+		mammoccioBgToShow = mainMammoccio.querySelector( '.main-mammoccio__bg.' + name ),
+		mammoccioContentToShow = mainMammoccio.querySelector( '.mammoccio-content.' + name ),
+		mammoccioBgToKill = mainMammoccio.querySelector( '.main-mammoccio__bg.current' ),
+		mammoccioContentToKill = mainMammoccio.querySelector( '.mammoccio-content.current' );
+	if ( mammoccioBgToKill && mammoccioBgToKill != mammoccioBgToShow ) {
+		mammoccioBgToKill.classList.remove( 'current' );
+		mammoccioContentToKill.classList.remove( 'current' );
+		mammoccioBgToShow.classList.add( 'current' );
+		mammoccioContentToShow.classList.add( 'current' );
+		mainMammoccio.style.height = mammoccioContentToShow.offsetHeight + 40 + 'px';
+	}
+}
+
+function handleMammocci() {
+	var mammocci = document.querySelectorAll( '.mammoccio' );
+	if ( mammocci ) {
+		mainMammoccio.style.height = mainMammoccio.querySelector( '.mammoccio-content.current' ).offsetHeight + 40 + 'px';
+		[].forEach.call( mammocci, function ( mammoccio ) {
+			mammoccio.addEventListener( 'click', updateMammoccio, false);
+		});
+	}
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+	// FastClick magic
+	FastClick.attach(document.body);
+	responsi.init();
+	handleMammocci();
 	menuHandler();
 }, false);
