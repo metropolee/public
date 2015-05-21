7d4239b8a18eec7bb9081865564392b7a787a708
Emanuele <feliziani.emanuele@gmail.com>
2015-05-21 20:54:25 +0200

    Add the FAQ page and fix other stuff
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -56,9 +56,81 @@ function handleMammocci() {
 	}
 }
 
+/***************** scrollTo Helper *****************/
+// Ubercool scrollTo animation taken from here: https://gist.github.com/james2doyle/5694700. It uses requestAnimationFrame for better performance. Ubercool!
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) {
+		return c/2*t*t + b;
+	}
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+// requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
+var requestAnimFrame = (function(){
+	return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); };
+})();
+
+function scrollTo(to, callback, duration) {
+	// because it's so fucking difficult to detect the scrolling element, just move them all
+	function move(amount) {
+		document.documentElement.scrollTop = amount;
+		document.body.parentNode.scrollTop = amount;
+		document.body.scrollTop = amount;
+	}
+	function position() {
+		return document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
+	}
+	var start = position(),
+		change = to - start,
+		currentTime = 0,
+		increment = 20;
+	duration = (typeof(duration) === 'undefined') ? 500 : duration;
+	var animateScroll = function() {
+		// increment the time
+		currentTime += increment;
+		// find the value with the quadratic in-out easing function
+		var val = Math.easeInOutQuad(currentTime, start, change, duration);
+		// move the document.body
+		move(val);
+		// do the animation unless its over
+		if (currentTime < duration) {
+			requestAnimFrame(animateScroll);
+		} else {
+			if (callback && typeof(callback) === 'function') {
+				// the animation is done so lets callback
+				callback();
+			}
+		}
+	};
+	animateScroll();
+}
+
+function scrollToInit() {
+	var hashLinks = document.querySelectorAll( 'a[href^="#"]' );
+	if ( hashLinks ) {
+		[].forEach.call( hashLinks, function ( hashLink ) {
+			hashLink.addEventListener('click', function(event) {
+				event.preventDefault();
+				var target = this.getAttribute('href').substring(1);
+				if ( target ) {
+					scrollTo( document.getElementById( target ).offsetTop - 150, 400);
+				}
+			}, false);
+		});
+	}
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	// FastClick magic
 	FastClick.attach(document.body);
 	handleMammocci();
 	menuHandler();
+	scrollToInit();
 }, false);
