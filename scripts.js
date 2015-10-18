b0e60f09557926ae59fbd0adf0239e5775892ff7
Emanuele <feliziani.emanuele@gmail.com>
2015-10-18 17:30:24 +0200

    Improve static files handling
    - Load Google fonts over https
    - Load old scripts separately because that's why it's done on the live site for now; the js file is called scripts-initial-compiled.js
    - The new scripts have been improved to handle the absence of video elements (was throwing an error to the console)
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,23 +1,29 @@
 
 function handleVideoPlayer() {
 	var videoTriggers = document.querySelectorAll( '.video-trigger' ),
-		videoPlayerContainer = document.querySelector( '.video-player-container' ),
-		videoPlayerClose = videoPlayerContainer.querySelector( '.video-player-close' ),
-		videoIframe = videoPlayerContainer.querySelector( 'iframe' );
+		videoPlayerContainer = document.querySelector( '.video-player-container' );
 	
-	[].forEach.call( videoTriggers, function ( videoTrigger ) {
-		videoTrigger.addEventListener('click', function( event ) {
-			event.preventDefault();
-			var videoUrl = 'https://www.youtube.com/embed/' +  this.getAttribute( 'href' ).split("/").pop() + '?autoplay=1&showinfo=0&rel=0&wmode=transparent';
-			
-			videoIframe.setAttribute( 'src', videoUrl );
-			videoPlayerContainer.classList.add( 'open' );
-			videoPlayerClose.addEventListener('click', function() {
-				videoPlayerContainer.classList.remove( 'open' );
-				videoIframe.removeAttribute( 'src' );
-			}, false);
-		}, false);
-	});
+	if ( videoTriggers && videoPlayerContainer ) {
+		var videoPlayerClose = videoPlayerContainer.querySelector( '.video-player-close' ),
+			videoIframe = videoPlayerContainer.querySelector( 'iframe' );
+		
+		[].forEach.call( videoTriggers, function ( videoTrigger ) {
+			if ( videoTrigger ) {
+				console.log(videoTrigger);
+				videoTrigger.addEventListener('click', function( event ) {
+					event.preventDefault();
+					var videoUrl = 'https://www.youtube.com/embed/' +  this.getAttribute( 'href' ).split("/").pop() + '?autoplay=1&showinfo=0&rel=0&wmode=transparent';
+					
+					videoIframe.setAttribute( 'src', videoUrl );
+					videoPlayerContainer.classList.add( 'open' );
+					videoPlayerClose.addEventListener('click', function() {
+						videoPlayerContainer.classList.remove( 'open' );
+						videoIframe.removeAttribute( 'src' );
+					}, false);
+				}, false);
+			}
+		});
+	}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
