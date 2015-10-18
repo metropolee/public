72017669f7be4641ad7d8e0e07c39a0eedf9a004
Emanuele <feliziani.emanuele@gmail.com>
2015-10-19 01:33:15 +0200

    New handling for smart-app-banner
    I modified the smart-app-banner thing because I am not able to modify the `<head>` of the live site. I am now creating the meta tags and all via js and also concatenating scripts and styles in my build process. There are also style fixes for the smart-app-banner.
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+document.querySelector('head').innerHTML += '<meta name="apple-itunes-app" content="app-id=1031598275">' +
+	'<meta name="google-play-app" content="app-id=com.metropolee.app.app55a2590531c3d">' +
+	'<!-- for Facebook -->' +
+	'<meta property="og:title" content="Metropolee" />' +
+	'<meta property="og:type" content="website" />' +
+	'<meta property="og:image" content="https://metropolee.s3.amazonaws.com/images/favicons/metropolee-logo.png" />' +
+	'<meta property="og:url" content="http://metropolee.com" />' +
+	'<meta property="og:description" content="Metropolee lets you build smartphone applications for local businesses and make profits out of them." />' +
+	'<meta property="og:site_name" content="Metropolee" />' +
+	'<!-- for Twitter -->' +
+	'<meta name="twitter:card" content="summary" />' +
+	'<meta name="twitter:site" content="@metropoleeapp" />' +
+	'<meta name="twitter:title" content="Metropolee" />' +
+	'<meta name="twitter:description" content="Metropolee lets you build smartphone applications for local businesses and make profits out of them." />' +
+	'<meta name="twitter:image" content="https://metropolee.s3.amazonaws.com/images/favicons/metropolee-logo.png" />';
+
+new SmartBanner({
+	daysHidden: 15,
+	daysReminder: 90,
+	title: 'Metropolee Network App',
+	author: 'Metropolee',
+	button: 'VIEW',
+	store: {
+		ios: 'On the App Store',
+		android: 'In Google Play',
+	},
+	price: {
+		ios: 'FREE',
+		android: 'FREE',
+	}
+//	, force: 'ios'
+});
 
 function handleVideoPlayer() {
 	var videoTriggers = document.querySelectorAll( '.video-trigger' ),
