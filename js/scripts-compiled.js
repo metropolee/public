9b12a18cc2e725e1ba1e706963a4a6b382c91e04
Emanuele <feliziani.emanuele@gmail.com>
2015-11-24 16:42:37 +0100

    Header scroll fix
    Header scroll fix (the calculation was being run multiple times because it was badly defined). I also add an initial run of the function so it is updated when the user does not scroll (the browser might preserve sroll location within refreshes).
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- js/scripts-compiled.js
+++ js/scripts-compiled.js
@@ -1026,7 +1026,7 @@ function updateHeader() {
 	} else if ( headerScrolled && movingUp ) {
 		header.classList.remove( 'mod-scrolled' );
 		headerScrolled = false;
-	} else if ( !headerScrolled && currentScrollY > 20 ) {
+	} else if ( !headerScrolled && currentScrollY > 50 ) {
 		header.classList.add( 'mod-scrolled' );
 		headerScrolled = true;
 	}
@@ -1038,14 +1038,14 @@ function updateHeader() {
 function update() {
 	movingUp = false;
 	movingDown = false;
-	if ( previousScrollY < currentScrollY && currentScrollY > 40 ) {
+	if ( previousScrollY < currentScrollY && currentScrollY > 50 ) {
 		// We're scrolling down
 		movingDown = true;
 		updateHeader();
 	} else {
 		// We're scrolling up
 		movingUp = true;
-		if ( currentScrollY < 60 ) {
+		if ( currentScrollY < 50 ) {
 			updateHeader();
 		}
 	}
@@ -1113,6 +1113,7 @@ function cleanSignup() {
 cleanSignup();
 
 document.addEventListener('DOMContentLoaded', function() {
+	updateHeader();
 	document.addEventListener( 'scroll', onScroll, false );
 	handleVideoPlayer();
 }, false);
